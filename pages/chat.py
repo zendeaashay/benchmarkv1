796f1dd4548c2767fe274ed1d61a0a5b3b26bf03
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-03-03 15:12:23 -0500

    Create chat.py

---
+++ pages/chat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 15:10:24 2024
+
+@author: aashayzende
+"""
+
+import streamlit as st
+from streamlit_chat import message
+
+message("My message") 
+message("Hello bot!", is_user=True)
